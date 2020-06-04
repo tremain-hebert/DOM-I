918807f98fb2de03a4233eff99b516f44fe505e7
Truman <truman@Trumans-iMac.local>
2020-06-03 23:30:34 -0500

    Nav Complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,36 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Navigation
+let nav1 = document.querySelector
+('nav > a:nth-child(1)');
+nav1.textContent = siteContent.nav
+['nav-item-1']
+
+let nav2 = document.querySelector
+('nav > a:nth-child(2)');
+nav2.textContent = siteContent.nav
+['nav-item-2']
+
+let nav3 = document.querySelector
+('nav > a:nth-child(3)');
+nav3.textContent = siteContent.nav
+['nav-item-3']
+
+let nav4 = document.querySelector
+('nav > a:nth-child(4)');
+nav4.textContent = siteContent.nav
+['nav-item-4']
+
+let nav5 = document.querySelector
+('nav > a:nth-child(5)');
+nav5.textContent = siteContent.nav
+['nav-item-5']
+
+let nav6 = document.querySelector
+('nav > a:nth-child(6)');
+nav6.textContent = siteContent.nav
+['nav-item-6']
+
+
